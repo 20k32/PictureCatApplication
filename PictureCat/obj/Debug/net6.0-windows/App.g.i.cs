1ec06e81a3c5a0704056bdde211743607cf5d422
20k32 <egormobtel@gmail.com>
2023-04-16 18:38:19 +0300

    Attached gitignore file for unnecessary folders and files

--- PictureCat/obj/Debug/net6.0-windows/App.g.i.cs
+++ PictureCat/obj/Debug/net6.0-windows/App.g.i.cs
@@ -47,7 +47,7 @@ namespace PictureCat {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.5.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -59,7 +59,7 @@ namespace PictureCat {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/PictureCat;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/PictureCat;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -73,7 +73,7 @@ namespace PictureCat {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.5.0")]
         public static void Main() {
             PictureCat.App app = new PictureCat.App();
             app.InitializeComponent();
