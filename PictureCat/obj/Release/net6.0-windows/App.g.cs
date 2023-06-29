5cde84a705621651132885c0a3f3e253d0088ecf
20k32 <egormobtel@gmail.com>
2023-06-29 18:19:57 +0300

    added demonstration gif file

--- PictureCat/obj/Release/net6.0-windows/App.g.cs
+++ PictureCat/obj/Release/net6.0-windows/App.g.cs
@@ -47,7 +47,7 @@ namespace PictureCat {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.5.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -73,7 +73,7 @@ namespace PictureCat {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.5.0")]
         public static void Main() {
             PictureCat.App app = new PictureCat.App();
             app.InitializeComponent();
